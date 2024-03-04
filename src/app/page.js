c88614985a3510b2747baa9d8da7f5fd749d0777
gabaal <geraldbaalham@live.co.uk>
2024-03-04 08:32:49 +0000

    Added framer-motion to home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,19 @@
-
+'use client'
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
     <div>
     <img src="/aquarium.jpg" placeholder="blur" className="background-container"></img>
+    <motion.div 
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    exit={{ opacity: 0 }}
+    transition={{ duration: 1.5 }}
+    >
     <h1 className="overlay-text">Welcome to Fish Talk, the website for tropical aquariums.</h1>
+    </motion.div>
+    
     </div>
   );
 }
